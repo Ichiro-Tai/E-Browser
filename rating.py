344c2086ad20d4f3f59f00083a4d6f378545c645
Derril <IDTai@hotmail.com>
2017-09-23 17:37:20 -0500

    native messaging added

--- rating.py
+++ rating.py
@@ -1,10 +1,13 @@
+#! /usr/bin/python
+
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import sys
+import json
 
 sid = SentimentIntensityAnalyzer()
 
 
 def rate_positivity():
-    text = sys.stdin.read()
-    # return sid.polarity_scores(text)
-    return 'tears.png'
+    text = json.loads(sys.stdin.read())
+    score = sid.polarity_scores(text)
+    return json.dumps('tears.png')
