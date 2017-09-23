7f7a29c11e0be2ad4b14d9fb03119d4edf994f83
Derril <IDTai@hotmail.com>
2017-09-23 18:29:31 -0500

    rating.py: adjusted stdin and stdout

--- rating.py
+++ rating.py
@@ -3,11 +3,32 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import sys
 import json
+import struct
 
 sid = SentimentIntensityAnalyzer()
 
 
+def getMessage():
+    rawLength = sys.stdin.read(4)
+    if len(rawLength) == 0:
+        sys.exit(0)
+    messageLength = struct.unpack('@I', rawLength)[0]
+    message = sys.stdin.read(messageLength)
+    return json.loads(message)
 
-# text = json.loads(sys.stdin.read())
-# score = sid.polarity_scores(text)
-sys.stdout.write(json.dumps('tears.png'))
+
+def encodeMessage(messageContent):
+    encodedContent = json.dumps(messageContent)
+    encodedLength = struct.pack('@I', len(encodedContent))
+    return {'length': encodedLength, 'content': encodedContent}
+
+
+def sendMessage(encodedMessage):
+    sys.stdout.write(encodedMessage['length'])
+    sys.stdout.write(encodedMessage['content'])
+    sys.stdout.flush()
+
+while True:
+    receivedMessage = getMessage()
+    score = sid.polarity_scores(receivedMessage)
+    sendMessage(encodeMessage('tears.png'))
