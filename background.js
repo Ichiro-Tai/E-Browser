5acf824fa1067646ab0ac2400a1409d1403af402
zhuyifei1999 <zhuyifei1999@gmail.com>
2017-09-23 19:40:58 -0500

    Programmatically create the popup html
    This will leak memory, better solution is pending

--- background.js
+++ background.js
@@ -5,8 +5,12 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
 		})
 			.then((text) => browser.runtime.sendNativeMessage('rating', text))
 			.then((uri) => {
-				browser.pageAction.setIcon({
-					tabId: tabId, path: uri
+				browser.pageAction.setIcon({tabId: tabId, path: uri});
+				browser.pageAction.getPopup({tabId: tabId}, (oldpopup) => {
+					var popup = URL.createObjectURL(new Blob(['abcdefg'], {type: 'text/html'}));
+					browser.pageAction.setPopup({tabId: tabId, popup: popup});
+					if (oldpopup)
+						URL.revokeObjectURL(oldpopup);
 				});
 				browser.pageAction.show(tabId);
 			})
