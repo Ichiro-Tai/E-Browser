46614bb48cc228469fd9da4dbe3703c38d6a22ee
zhuyifei1999 <zhuyifei1999@gmail.com>
2017-09-23 19:01:26 -0500

    background.js: Some debugging cleanup

--- background.js
+++ background.js
@@ -4,16 +4,13 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
 			code: 'document.body.innerText'
 		})
 			.then((text) => browser.runtime.sendNativeMessage('rating', text))
-			.catch((error) => console.log('Error B: ' + error)) // eslint-disable-line no-console
 			.then((uri) => {
-				console.log(uri);
 				browser.pageAction.setIcon({
 					tabId: tabId, path: uri
 				});
 				browser.pageAction.show(tabId);
 			})
-			.catch((error) => console.log('Error A: ' + error)); // eslint-disable-line no-console
-			// .catch((error) => browser.tabs.executeScript(tabId, {code: error}));
+			.catch((error) => console.log('Error: ' + error)); // eslint-disable-line no-console
 	} else {
 		browser.pageAction.hide(tabId);
 	}
